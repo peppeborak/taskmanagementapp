5254b0479cf5af096cecf9299c1786d4f89e72a0
peppeborak <peppe@borak.se>
2024-12-17 12:27:28 +0100

    Fixed bugs in task-delete-handler
    - Added return statements

--- handlers/task-delete-handler.js
+++ handlers/task-delete-handler.js
@@ -7,23 +7,23 @@ export const taskDeleteHandler = async (req, res) => {
 
     // Validate userId
     if (!userId || isNaN(userId)) {
-      res.send(400).json({ message: 'Task id is required' })
+      return res.status(400).json({ message: 'Task id is required' })
     }
 
     // Validate taskId
     if (!taskId || isNaN(taskId)) {
-      res.send(400).json({ message: 'Task id is required' })
+      return res.status(400).json({ message: 'Task id is required' })
     }
 
     // Query database
     const rowsDeleted = await taskDeleteDb(userId, taskId)
     // Check if query was successful
     if (rowsDeleted === 0) {
-      res.status(400).json({ message: 'Failed to delete task' })
+      return res.status(400).json({ message: 'Failed to delete task' })
     }
 
     // Respond with a success message
-    res.status(200).json({ message: 'Successfully deleted task' })
+    return res.status(200).json({ message: 'Successfully deleted task' })
   } catch (err) {
     console.error(err)
     return res.status(500).json({ message: 'Internal Server Error' })
