7596d1ae08044e07d1b253ad2cd02bd03eb7ab22
peppeborak <peppe@borak.se>
2024-12-17 11:00:26 +0100

    Updated list-update-handler
    - Changed newListName variable
    - Removed fetch list query
    - Returns only updated name instead of the whole object
    - Does 1 query / job now instead of 2

--- handlers/list-update-handler.js
+++ handlers/list-update-handler.js
@@ -1,36 +1,28 @@
-import { listUpdateDb, listFetchOneDb } from '../utils/db-queries.js'
+import { listUpdateDb } from '../utils/db-queries.js'
 
 export const listUpdateHandler = async (req, res) => {
   try {
     const listId = req.params.id
-    const { listName } = req.body
+    const { newListName } = req.body
     const userId = req.user.id
 
-    // Validate list
-    if (!listName || listName.trim() === '') {
-      return res.status(400).json({ message: 'List name is required' })
-    }
-
-    // Validate listName
+    // Validate listId
     if (!listId || isNaN(listId)) {
       return res.status(400).json({ message: 'List id is required' })
     }
 
-    // Validate userId
-    if (!userId || isNaN(userId)) {
-      return res.status(400).json({ message: 'User id is required' }  )
+    // Validate newListName
+    if (!newListName || newListName.trim() === '') {
+      return res.status(400).json({ message: 'List name is required' })
     }
 
-    // Fetch the list
-    const list = await listFetchOneDb(userId, listId)
-
-    // Check if list exists
-    if (!list) {
-      return res.status(400).json({ message: 'List does not exist' })
+    // Validate userId
+    if (!userId || isNaN(userId)) {
+      return res.status(400).json({ message: 'User id is required' })
     }
 
     // Query database
-    const rowsAffected = await listUpdateDb(listName, userId, listId)
+    const rowsAffected = await listUpdateDb(newListName, userId, listId)
 
     // Check if query was successful
     if (rowsAffected === 0) {
@@ -42,7 +34,7 @@ export const listUpdateHandler = async (req, res) => {
     // Respond with the updated list name
     return res.status(200).json({
       message: 'List updated successfully',
-      updatedName: listName,
+      updatedName: newListName,
     })
   } catch (err) {
     console.error(err)
