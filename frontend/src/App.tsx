73e9b7faead1a0e57c8de16bb8d826809e0575dd
peppeborak <peppe@borak.se>
2024-12-18 13:35:07 +0100

    Created pages
    - Login
    - Signup
    - Dashboard
    - Notfound

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login } from './routes/Login'
+import { Signup } from './routes/Signup'
+import { Notfound } from './routes/Notfound'
 
 function App() {
-
-
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Login />}>
+          </Route>
+          <Route path="/signup" element={<Signup />}>
+          </Route>
+          <Route path="/*" element={<Notfound />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
