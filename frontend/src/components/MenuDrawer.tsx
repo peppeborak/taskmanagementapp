63ff1cc03a71e9879c0a4f71d47e1b111018146b
peppeborak <peppe@borak.se>
2024-12-30 14:19:08 +0100

    Added MenuDrawer.tsx

---
+++ frontend/src/components/MenuDrawer.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import List from '@mui/material/List'
+import Divider from '@mui/material/Divider'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import { IconButton } from '@mui/material'
+import RestoreFromTrashIcon from '@mui/icons-material/RestoreFromTrash'
+import SettingsIcon from '@mui/icons-material/Settings'
+import MenuIcon from '@mui/icons-material/Menu'
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+
+export default function TemporaryDrawer() {
+  const [open, setOpen] = React.useState(false)
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen)
+  }
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AccountBoxIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Profile'} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <RestoreFromTrashIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Deleted tasks'} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Settings'} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  )
+
+  return (
+    <div>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+        onClick={toggleDrawer(true)}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  )
+}
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,28 +1,18 @@
-import { Box, IconButton, Toolbar, Typography } from '@mui/material'
+import { Box, Toolbar, Typography } from '@mui/material'
 import AppBar from '@mui/material/AppBar'
-
-import MenuIcon from '@mui/icons-material/Menu'
+import MenuDrawer from './MenuDrawer'
 import { LogoutButton } from './LogoutButton'
 
 export const NavBar = () => {
   return (
-    <Box 
-    sx={{ flexGrow: 'row' }}>
+    <Box sx={{ flexGrow: 'row' }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          <MenuDrawer />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Task Management App
           </Typography>
-          <LogoutButton/>
+          <LogoutButton />
         </Toolbar>
       </AppBar>
     </Box>
