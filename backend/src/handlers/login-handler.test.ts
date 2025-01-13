4e1e2465e6b18bd1d920ff4b9b86af236e09e98d
peppeborak <peppe@borak.se>
2025-01-13 23:53:15 +0100

    updated login-handler.test.ts
    - Added scenario when user does not exist

--- backend/src/handlers/login-handler.test.ts
+++ backend/src/handlers/login-handler.test.ts
@@ -1,12 +1,13 @@
 import supertest from 'supertest'
 import { app } from '../server'
 import bcrypt from 'bcrypt'
+import * as dbQueries from '../utils/db-queries'
 
 jest.mock('../utils/db-queries', () => {
   return {
     getUserDb: jest
       .fn()
-      .mockReturnValue({ email: 'peppe@test.com', passwordHash: 'peppe123' }),
+      .mockReturnValue({ email: 'test@test.com', passwordHash: 'test' }),
   }
 })
 
@@ -18,30 +19,36 @@ const hashSpy = jest
 const compareSpy = jest.spyOn(mockBcrypt, 'compare').mockReturnValue(true)
 
 describe('POST /api/v1/login', () => {
-  it('should return successful login', async () => {
-    const email = 'peppe@test.com'
-    const password = 'peppe123'
-
+  it('should return 200 and successful login', async () => {
     const response = await supertest(app)
       .post('/api/v1/login')
-      .send({ email: email, password: password })
+      .send({ email: 'test@test.com', password: 'test123' })
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({
-      email: 'peppe@test.com',
+      email: 'test@test.com',
       message: 'Login successful!',
       token: expect.any(String),
     })
   })
 
-  it('should return incorrect password', async () => {
-    const email = 'peppe@test.com'
-    const password = 'peppe123'
+  it('should return 404 and user not found', async () => {
+    jest
+      .spyOn(dbQueries, 'getUserDb')
+      .mockReturnValueOnce(Promise.resolve(undefined as any))
 
+    const response = await supertest(app)
+      .post('/api/v1/login')
+      .send({ email: 'test@test.com', password: 'test' })
+
+    expect(response.statusCode).toBe(404)
+  })
+
+  it('should return 401 and incorrect password', async () => {
     compareSpy.mockReturnValueOnce(false)
     const response = await supertest(app)
       .post('/api/v1/login')
-      .send({ email: email, password: password })
+      .send({ email: 'test@test.com', password: 'test' })
 
     expect(response.statusCode).toBe(401)
     expect(response.body).toEqual({ message: 'Invalid password' })
